eded5b3953aef6df75e814b8889d3928efc43f7d
Maksym Karamanov <karamanov.maksym@ukr.net>
2021-10-31 06:44:10 +0200

    Load all photos from API service

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { getAllPhotos } from './services/api'
+
+type PhotoType = {
+  albumId: number
+  id: number
+  title: string
+  url: string
+  thumbnailUrl: string
+}
 
 function App() {
+
+  const [photos, setPhotos] = useState<PhotoType[] | null>(null)
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const photos = await getAllPhotos()
+        setPhotos(photos)
+      } catch (error) {
+        console.log('Request photo error', error)
+      }
+    }
+    run()
+  }, [])
+
   return (
     <div className="App">
     </div>
